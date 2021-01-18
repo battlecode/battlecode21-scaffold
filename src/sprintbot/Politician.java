85f4cef39ee63f7d75b43a9fa71a85cdfb5e75ed
colin <colinsullivan203@gmail.com>
2021-01-18 18:15:59 -0500

    updated polititicans

--- src/sprintbot/Politician.java
+++ src/sprintbot/Politician.java
@@ -82,60 +82,40 @@ public class Politician {
             Pathfinding3.moveTo(loc);
         }
 
-        // if near target loc and has no tracked ec, return true
-        return rc.getLocation().isWithinDistanceSquared(loc, 6) && trackedEC == -1;
+        // if in mission section and has no tracked ec, return true
+        return inMissionSection() && trackedEC == -1;
     }
 
     // hunt for a muck defensively at a given location
     // return false if a muck is not found
     public static boolean huntMuck(MapLocation loc) throws GameActionException {
-        // have not yet identified muck to destroy
-        RobotInfo[] nearbyRobots = rc.senseNearbyRobots(25); 
-        boolean tracking = false; 
-
+        int closestMuckDist = Integer.MAX_VALUE;
+        MapLocation closestMuckLoc = null;
+        RobotInfo[] nearbyRobots = rc.senseNearbyRobots(); 
         for(int i = nearbyRobots.length - 1; i >= 0; i--) { 
             RobotInfo robot = nearbyRobots[i]; 
-            if(robot.type == RobotType.MUCKRAKER && robot.team != rc.getTeam()) {
-                if(robot.location.distanceSquaredTo(rc.getLocation()) <= 9)
-                    rc.empower(robot.location.distanceSquaredTo(rc.getLocation()));
-                else {
-                    Pathfinding3.moveTo(robot.location); 
-                    tracking = true; 
-                    break;
-                }
-                
-            }
-
-        }
-
-        if(!tracking) Pathfinding3.moveTo(loc); 
-
-    /*    if(trackedMuck == -1) {
-            RobotInfo[] nearbyRobots = rc.senseNearbyRobots();
-            for(int i = nearbyRobots.length - 1; i >= 0; i--) {
-                RobotInfo robot = nearbyRobots[i]; 
-                if(robot.getTeam() != rc.getTeam() && robot.getType() == RobotType.MUCKRAKER) {
-                    trackedMuck = robot.ID;
-                    break;
-                }
+            if(robot.type == RobotType.MUCKRAKER && robot.team != rc.getTeam() && rc.getLocation().isWithinDistanceSquared(robot.location, closestMuckDist - 1)) {
+                closestMuckLoc = robot.location;
+                closestMuckDist = rc.getLocation().distanceSquaredTo(robot.location);
             }
         }
-        if(trackedMuck != -1 && rc.canSenseRobot(trackedMuck)) {
-            // move toward tracked muck
-            RobotInfo muck = rc.senseRobot(trackedMuck); 
-            if(rc.getLocation().isWithinDistanceSquared(muck.location, 2) && rc.canEmpower(2)) {
-                rc.empower(2);
+
+        if (closestMuckLoc != null) {
+            if (closestMuckDist <= 9 && rc.canEmpower(closestMuckDist)) {
+                rc.empower(closestMuckDist);
+                return true;
             } else {
-                Pathfinding3.moveTo(muck.location); 
+                Pathfinding3.moveTo(closestMuckLoc);
+                return false;
             }
         } else {
-            // reset tracked muck and move toward loc
-            trackedMuck = -1;
             Pathfinding3.moveTo(loc);
-        } */
+            return inMissionSection();
+        }
+    }
 
-        // if near target loc and has no tracked muck, return true
-        return rc.getLocation().isWithinDistanceSquared(loc, 6) && trackedMuck == -1;
+    private static boolean inMissionSection() {
+        return Communication.getCurrentSection().equals(missionSectionLoc);
     }
     
 }
