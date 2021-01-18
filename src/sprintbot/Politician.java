66cbc6679415b4199efeb28c28eb7c3684560e11
Trevleon <trev.leon7@gmail.com>
2021-01-17 23:08:45 -0600

    politician method for hunting muckrakers defensively

--- src/sprintbot/Politician.java
+++ src/sprintbot/Politician.java
@@ -3,6 +3,7 @@ import battlecode.common.*;
 
 public class Politician {
     static RobotController rc;
+    public static int trackedMuck = -1; 
 
     public static void run() throws GameActionException {
         int turn = 0;
@@ -10,8 +11,8 @@ public class Politician {
         initialize();
         while (true) {
             Communication.updateIDList(false);
-            Communication.updateSectionMissionInfo();
-            executeTurn(turn++);
+            Communication.updateSectionMissionInfo();            
+            executeTurn(turn++);            
             Clock.yield();
         }
     }
@@ -25,15 +26,32 @@ public class Politician {
     }
     //hunt for a muck defensively at a given location
     public static void huntMuck(MapLocation loc) throws GameActionException {
-        if(rc.getLocation().compareTo(loc) != 0) {
-            moveTo(loc); 
-            return false;
-        }
-        else {
-
         
+        //have not yet identified muck to destroy
+        if(trackedMuck == -1){
+            if(rc.getLocation().compareTo(loc) != 0) {
+                moveTo(loc); 
+                return false;
+            }
+            else {
+                RobotInfo[] nearbyRobots = senseNearbyRobots(EXPOSE_RADIUS);
+                for(int i = nearbyRobots.length - 1; x >= 0; x--) {
+                    RobotInfo robot = nearbyRobots[i]; 
+                    if(getType(robot) == RobotType.MUCKRAKER) {
+                        trackedMuck = robot.ID; 
+                    }
+                }
+            }
+        //have identified muck to destroy
+        } else {
+            if(rc.canSenseRobot(trackedMuck)) {
+                RobotInfo muck = rc.senseRobot(trackedMuck); 
+                if(rc.getLocation().distanceSquaredTo(muck.location) == 2 && rc.canEmpower(2)) {
+                    rc.empower(2);
+                }
+                else Pathfinding3.moveTo(muck.location); 
+            }
+            else trackedMuck = -1; 
+    
         }
-
-
-    }
 }
