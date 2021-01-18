4c30efe15dba99b49e13a54bc5bc856492fc2d54
Trevleon <trev.leon7@gmail.com>
2021-01-18 00:48:40 -0600

    added mission logic for mucks

--- src/sprintbot/Muckraker.java
+++ src/sprintbot/Muckraker.java
@@ -7,6 +7,13 @@ public class Muckraker {
     static final int MAX_SENSE_RADIUS = 30; 
     static int foundPoli = -1; 
     static MapLocation startLoc;
+    static int state; 
+    static final int STATE_UNKNOWN = 0;
+    static final int STATE_SLUETH = 1;
+    static final int STATE_SCOUT = 2;
+    static final int STATE_STICK = 3;
+    static MapLocation missionLoc; 
+
 
     public static void run() throws GameActionException {
         int turn = 0;
@@ -16,20 +23,78 @@ public class Muckraker {
             Communication.updateIDList(false);
             Communication.updateSectionMissionInfo();
             Communication.sendSectionInfo();
+            checkForMissions(); 
             executeTurn(turn++);
             Clock.yield();
         }
     }
+
+    private static boolean checkForMissions() {
+        if (state != STATE_UNKNOWN) return false;
+        MapLocation sectionLoc = Communication.getCurrentSection();
+        for (int xDiff = -5; xDiff <= 5; xDiff++) {
+            for (int yDiff = -5; yDiff <= 5; yDiff++) {
+                int sectionX = sectionLoc.x + xDiff;
+                int sectionY = sectionLoc.y + yDiff;
+                if (sectionX < 0 || sectionY < 0 || sectionX >= Communication.NUM_SECTIONS || sectionY >= Communication.NUM_SECTIONS) continue;
+                switch(Communication.getMissionTypeInSection(sectionX, sectionY)){
+                    case Communication.MISSION_TYPE_SLEUTH:
+                        missionLoc = Communication.getSectionCenter(sectionX, sectionY); 
+                        state = STATE_SLUETH
+                        return true;
+                        break;
+                    case Communication.MISSION_TYPE_SCOUT:
+                        missionLoc = Communication.getSectionCenter(sectionX, sectionY); 
+                        state = STATE_SCOUT
+                        return true;
+                        break;
+                    case Communication.MISSION_TYPE_STICK:
+                        missionLoc = Communication.getSectionCenter(sectionX, sectionY); 
+                        state = STATE_STICK
+                        return true;
+                        break;
+                    default: 
+                        break;  
+                }
+               
+            }
+        }
+        return false;
+    }
+    
 
     public static void initialize() throws GameActionException {
 
     }
 
     public static void executeTurn(int turnNumber) throws GameActionException {
+        //States: sluething, scouting, sticking
+        switch(state) {
+            case STATE_SLUETH: 
+                if(huntSlanderer(missionLoc)) state = STATE_UNKNOWN;
+                break;
+            case STATE_STICK: 
+                if(stickToPoli(missionLoc)) state = STATE_UNKNOWN;
+                break;
+            case STATE_SCOUT: 
+                if(scouting(missionLoc)) state = STATE_UNKNOWN;
+                break;
+            default: 
+                break; 
         
+        }
     }
-
-    public static boolean huntSlanderer(MapLocation loc, int turnNumber) throws GameActionException {
+    private static boolean scouting(MapLocation loc) throws GameActionException {
+        if(!rc.getLocation().equals(loc)) {
+            Pathfinding3.moveTo(loc); 
+            return false; 
+        }
+        else {
+            return true;
+        }
+    
+    }
+    private static boolean huntSlanderer(MapLocation loc) throws GameActionException {
         if(rc.getLocation().compareTo(loc) != 0) {
             Pathfinding3.moveTo(loc);
             if(getRoundNum() % 3 == 0) {
@@ -43,7 +108,7 @@ public class Muckraker {
             return true;
         }
     }
-    public static void senseAndExpose() throws GameActionException {
+    private static void senseAndExpose() throws GameActionException {
         RobotInfo[] nearbyRobots = senseNearbyRobots(EXPOSE_RADIUS);
                 for(int i = nearbyRobots.length - 1; x >= 0; x--) {
                     RobotInfo robot = nearbyRobots[i]; 
@@ -53,7 +118,7 @@ public class Muckraker {
     }
     //Muckraker moves towards a location, and when it gets there, checks to see if there is a politician in a 10 r^2 area. 
     //If there is, it always attempts to move towards it until it can no longer sense it (either dead or out of range)
-    public static boolean stickToPoli(MapLocation loc, int turnNumber) throws GameActionException {
+    private static boolean stickToPoli(MapLocation loc) throws GameActionException {
         if(rc.getLocation().compareTo(loc) != 0) {
             Pathfinding3.moveTo(loc); 
             return false; 
@@ -70,7 +135,7 @@ public class Muckraker {
     }
     
     }
-    public static boolean trackPolitician() {
+    private static boolean trackPolitician() {
         if(canSenseRobot(foundPoli)) {
             RobotInfo mark = senseRobot(foundPoli);
             Pathfinding3.moveTo(mark.location); 
